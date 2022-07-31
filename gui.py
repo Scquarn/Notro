1471f99a581f95573db48371cf65a53a51f4d37f
Scquarn <Scquarn@gmail.com>
2022-07-31 13:49:07 +0200

    Working Version for Windows

--- gui.py
+++ gui.py
@@ -1,9 +1,6 @@
 import PySimpleGUI as Sg
-from util import get_search_result, get_images
-from io import BytesIO
-import win32clipboard
-from PIL import Image
-import io
+from util import get_search_result, get_images, download_url, resize_emote, copy_to_clipboard, delete_file_delayed, \
+    delete_leftovers
 
 
 class GUI:
@@ -13,6 +10,7 @@ class GUI:
         self.layout = []
         self.window = None
         self.source = "FFZ"
+        self.sleep_time = 60
         self.cache = {}
 
         self.display_search()
@@ -20,19 +18,20 @@ class GUI:
     def main_loop(self):
         while True:
             event, values = self.window.read()
-            print(event, values)
             if event == Sg.WIN_CLOSED or event == "Cancel":
                 break
             if event == "btnSearch":
                 self.search_button(values["inpSearch"])
             if "btnEmote" in event:
                 self.emote_button(event)
+        delete_leftovers()
         self.window.close()
 
     def display_search(self, default_text=""):
         if self.window:
             self.window.close()
-        self.layout = [[Sg.InputText(default_text, focus=True, key="inpSearch"), Sg.Button("Search", key="btnSearch", bind_return_key=True)]]
+        self.layout = [[Sg.InputText(default_text, focus=True, key="inpSearch"),
+                        Sg.Button("Search", key="btnSearch", bind_return_key=True)]]
         self.window = Sg.Window(self.name, self.layout, finalize=True, use_default_focus=True)
         self.window.TKroot.focus_force()
         self.window.Element("inpSearch").SetFocus()
@@ -80,16 +79,14 @@ class GUI:
         self.display_images_as_grid(images)
 
     def emote_button(self, event):
-        self.display_search()
         emote_id = str(event).split("-")[1]
+        emote_filename = f"__tmp__Notro-{emote_id}.png"
 
-        image = Image.open(io.BytesIO(self.cache[emote_id]))
-        output = BytesIO()
-        image.convert('RGB').save(output, 'BMP')
-        data = output.getvalue()[14:]
-        output.close()
+        url = f"https://cdn.frankerfacez.com/emoticon/{emote_id}/2"
+        download_url(url, emote_filename)
+        resize_emote(emote_filename)
+        copy_to_clipboard(emote_filename)
 
-        win32clipboard.OpenClipboard()
-        win32clipboard.EmptyClipboard()
-        win32clipboard.SetClipboardData(win32clipboard.CF_DIB, data)
-        win32clipboard.CloseClipboard()
+        delete_file_delayed(emote_filename, self.sleep_time)
+
+        self.display_search()
--- util.py
+++ util.py
@@ -1,13 +1,30 @@
+import multiprocessing
+import threading
 import urllib
 import urllib.request
+import requests
 import json
 from multiprocessing import Pool
-import pyperclip
-
+import subprocess as Sp
+from PIL import Image, ImageFilter
+import os
+import time
 
 def image_from_url(url):
     response = urllib.request.urlopen(url)
     return response.read()
+
+
+def resize_emote(file_name):
+    im = Image.open(file_name).convert('RGBA')
+    im.thumbnail((48, 48), resample=Image.LANCZOS)
+    im = im.filter(ImageFilter.SHARPEN)
+    im.save(file_name)
+
+
+def copy_to_clipboard(file_name):
+    command = f"Set-Clipboard -Path .\{file_name}"
+    Sp.run(["powershell", "-Command", command], capture_output=True)
 
 
 def get_image(emote_id):
@@ -15,25 +32,40 @@ def get_image(emote_id):
     return image_from_url(url), emote_id
 
 
+def delete_file_delayed(filename, sleep_time=20):
+    deleter = threading.Thread(target=delete_file, args=(filename, sleep_time))
+    deleter.daemon = True
+    deleter.start()
+
+
+def delete_file(filename, sleep_time):
+    time.sleep(sleep_time)
+    if os.path.exists(filename):
+        os.remove(filename)
+
+
+def delete_leftovers():
+    for filename in os.listdir("."):
+        f = os.path.join(".", filename)
+        if os.path.isfile(f):
+            if "__tmp__Notro-" in f and ".png" in f:
+                os.remove(f)
+
+
+def download_url(url, file_name):
+    r = requests.get(url)
+    open(file_name, 'wb').write(r.content)
+    return
+
+
 def get_images(emotes):
     if not emotes:
         return
-    ids = list(zip(*emotes))[1]
-    with Pool(processes=16) as p:
-        images_and_ids = p.map_async(get_image, ids).get()
+    with Pool(processes=multiprocessing.cpu_count() * 2 + 2) as p:
+        images_and_ids = p.map(get_image, list(zip(*emotes))[1])
+        p.join()
+
     return images_and_ids
-
-
-def get_emote_url(emote_id, source):
-    if source == "FFZ":
-        pyperclip.copy(f"https://cdn.frankerfacez.com/emoticon/{emote_id}/1")
-    elif source == "7TV":
-        pyperclip.copy(f"https://cdn.frankerfacez.com/emoticon/{emote_id}/1")
-    elif source == "BTTV":
-        pyperclip.copy(f"https://cdn.frankerfacez.com/emoticon/{emote_id}/1")
-    else:
-        print("Specify correct source!")
-        return None
 
 
 def get_search_result(search_string, source):
@@ -49,18 +81,17 @@ def get_search_result(search_string, source):
 
 
 def get_emotes_7tv(search_string):
-    #url = f"https://7tv.app/emotes?sortBy=popularity&page=0&query={search_string}"
-    #driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-    #time.sleep(5)
-    #driver.get(url)
-    #time.sleep(5)
-    #page = driver.page_source
-    #page_soup = BeautifulSoup(page, 'html.parser')
-    #images = []
-    #for img in page_soup.find_all('img'):
-    #    images.append(img.get('src'))
+    # url = f"https://7tv.app/emotes?sortBy=popularity&page=0&query={search_string}"
+    # driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    # time.sleep(5)
+    # driver.get(url)
+    # time.sleep(5)
+    # page = driver.page_source
+    # page_soup = BeautifulSoup(page, 'html.parser')
+    # images = []
+    # for img in page_soup.find_all('img'):
+    #   images.append(img.get('src'))
     # print(images)
-    # exit(-1)
     print("Not implemented")
     exit(-1)
 
